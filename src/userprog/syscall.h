02e8a786063431f3709e24449a34b4f252b1ed8b
Sean Teramae <seteramae@gmail.com>
2018-10-17 21:17:29 -1000

    Cleaned up Code, added open and close sys

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,21 +2,4 @@
 #define USERPROG_SYSCALL_H
 #include "threads/synch.h"
 void syscall_init (void);
-
-struct child_process {
-  int pid;
-  int load;
-  bool wait;
-  bool exit;
-  int status;
-  struct lock wait_lock;
-  struct list_elem elem;
-};
-struct child_process* add_child_process (int pid);
-struct child_process* get_child_process (int pid);
-void remove_child_process (struct child_process *cp);
-void remove_child_processes (void);
-
-void process_close_file (int fd);
-void sys_exit (int status);
 #endif /* userprog/syscall.h */
